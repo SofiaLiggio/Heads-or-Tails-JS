a78a8afeeb672841b7326acd836b98755e48f5b5
sofia_liggio <sofialiggio994@gmail.com>
2024-08-30 18:04:39 +0200

    fix script

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let adversary = move[Math.trunc(Math.random() * move.length)].toUpperCase();
 
 if (player === adversary) {
   console.log(`WIN! IT'S ${adversary}`);
-} else if (player !== "heads" || player !== "tails") {
-  alert(`CHOOSE HEADS OR TAILS`);
-} else player !== adversary;
-console.log(`LOSE! IT'S ${adversary}`);
+} else {
+  player !== adversary;
+  console.log(`LOSE! IT'S ${adversary}`);
+}
